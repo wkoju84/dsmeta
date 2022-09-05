4c3c32633003c6996719b6565decaf79f451201d
wkoju84 <wkoju84@gmail.com>
2022-09-05 16:18:47 -0300

    UseState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 
 function App() {
@@ -20,4 +20,4 @@ function App() {
   )
 }
 
-        export default App
+        export default App;
